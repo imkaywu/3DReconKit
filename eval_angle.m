3ca0def98f6a1f99dd9bb0c5e48607c2cbabba04
Squidieee <chaine.zhou@gmail.com>
2017-04-27 15:43:38 -0700

    added vh

--- eval_angle.m
+++ eval_angle.m
@@ -1,18 +1,12 @@
 mask = imread(sprintf('%s/gt/mask.bmp', data.rdir));
-mask(mask > 128) = 1;
-norm_map_gt = imread(sprintf('%s/gt/normal.jpg', data.rdir));
-norm_map_gt = double(norm_map_gt);
-norm_map_gt(:, :, 1 : 2) = norm_map_gt(:, :, 1 : 2) * 2.0 / 255.0 - 1;
-norm_map_gt(:, :, 3) = norm_map_gt(:, :, 3) / 255.0;
-norm_map_gt(:, :, 2) = -norm_map_gt(:, :, 2);
-mask_0_ind = find(mask == 0);
-for i = 1 : 3
-    norm_map_gt(mask_0_ind + (i - 1) * numel(mask)) = 0;
-end
+mask(mask > 0) = 1;
+norm_map_rgb_gt = imread(sprintf('%s/gt/normal.jpg', data.rdir));
+norm_map_gt = decode(norm_map_rgb_gt, mask);
 show_surfNorm(255 * mask, norm_map_gt, 10);
 
 if(~exist('norm_map', 'var'))
-    norm_map = imread(sprintf('%s/normal.jpg', data.dir));
+    norm_map_rgb = imread(sprintf('%s/normal.jpg', data.dir));
+    norm_map = decode(norm_map_rgb, mask);
 end
 show_surfNorm(255 * mask, norm_map, 10);
 
@@ -28,18 +22,22 @@ for i = 1 : 3
     norm(:, i) = norm_tmp(mask == 1);
 end
 
+prct = 0.9;
 dot_norm = dot(norm_gt, norm, 2);
 angle = (180 .* acos(dot_norm)) ./ pi;
 angle = real(angle);
+angle_sort = sort(angle);
+acc_ang = angle_sort(round(prct * numel(angle)));
 mean_ang = mean(angle);
 var_ang = var(angle);
 std_ang = std(angle);
 max_ang = max(angle);
 
+fprintf('accuracy: %.08f\n', acc_ang);
 fprintf('mean angle: %.08f\n', mean_ang);
 fprintf('var angle: %.08f\n', var_ang);
 fprintf('std angle: %.08f\n', std_ang);
 fprintf('max angle: %.08f\n', max_ang);
 
-fid = fopen([dir, '/result.txt'], 'wt');
-fprintf(fid, 'mean angle: %.08f\nvar angle: %.08f\nstd angle: %.08f\nmax angle: %.08f\n', mean_ang, var_ang, std_ang, max_ang);
+fid = fopen([data.dir, '/result.txt'], 'wt');
+fprintf(fid, 'accuracy: %.08f\nmean angle: %.08f\nvar angle: %.08f\nstd angle: %.08f\nmax angle: %.08f\n', acc_ang, mean_ang, var_ang, std_ang, max_ang);
--- run.m
+++ run.m
@@ -1,4 +1,5 @@
 % run algorithm and compute the accuracy and completeness
+clear, clc, close all;
 addpath ('io');
 
 is_tex = 1;
@@ -8,9 +9,10 @@ is_rough = 1;
 is_concav = 1;
 
 obj_name = 'sphere';
-alg_type = 'ps';
+alg_type = 'vh';
+prop_comb = 'tex_spec';
 rdir = sprintf('C:/Users/Admin/Documents/3D_Recon/Data/synthetic_data/%s', obj_name);
-idir = sprintf('%s/tex_spec/%s', rdir, alg_type);
+idir = sprintf('%s/%s/%s', rdir, prop_comb, alg_type);
 ref_dir = '../ref_obj';
 gt_dir = '../groundtruth';
 update = 0;
@@ -37,13 +39,18 @@ end
 
 %% Run SL
 case 'sl'
-addpath C:/Users/Daniela/Documents/3D_Recon/kwStructuredLight
-objDir = sprintf('%s/%02d%02d/', idir, i, j);
-for i = 2 : 3 : 8
-    for j = 1 : 10
+dir_sl = 'C:/Users/Admin/Documents/3D_Recon/kwStructuredLight';
+addpath (genpath(dir_sl));
+for ind_tex = 2 : 3 : 8
+    for ind_spec = 2 : 3 : 8
         % change objName
-        slProcess_syn;
-        eval_ang;
+        objDir = sprintf('%s/%02d%02d', idir, ind_tex, ind_spec); % used in slProcess_syn
+        if(update || ~exist(sprintf('%s/%s_sl.ply', objDir, obj_name), 'file'))
+            slProcess_syn;
+        end
+        if(update || ~exist(sprintf('%s/result.txt', objDir), 'file'))
+            eval_acc_cmplt;
+        end
     end
 end
 
@@ -51,28 +58,30 @@ end
 case 'ps'
 dir_ps = 'C:/Users/Admin/Documents/3D_Recon/Photometric Stereo';
 addpath(genpath(dir_ps));
-
 for ind_tex = 5
     for ind_spec = 5
         data.dir = sprintf('%s/%02d%02d', idir, ind_tex, ind_spec);
         data.rdir = rdir;
         data.ref_dir = ref_dir;
         data.obj_name = obj_name;
-        main_ps;
+        if(~exist(sprintf('%s/normal.jpg', data.dir), 'file'))
+            main_ps;
+        end
         eval_angle;
     end
 end
 
 %% Run VH
 case 'vh'
-addpath C:/Users/Daniela/Documents/3D_Recon/Visual Hull Matlab;
-for i = 2 : 3 : 8
-    for j = 1 : 10
-        dir = sprintf('%s/%02d%02d/mvs_vh', idir, i, j); % used in VisualHullMain_syn
-        file_base = 'cup'; % name of the gt
-        VisualHullMain_syn; % write this sub-routine
-        evaluate;
-    end
+addpath C:\Users\Admin\Documents\3D_Recon\VisualHull;
+dir = sprintf('%s/vh', rdir); % used in VisualHullMain_syn
+file_base = obj_name; % name of the gt
+if(update || ~exist(sprintf('%s/%s_vh.ply', dir, obj_name), 'file'))
+    VisualHullMain_syn; % write this sub-routine
 end
+if(update || ~exist(sprintf('%s/result.txt', dir), 'file'))
+    eval_acc_cmplt;
+end
+
 
 end % end of switch statement
